1ecd940e69b4aeaf8f35f7348d63787ab0b019f2
Adam Giaccaglia <adamgcog@gmail.com>
2017-07-22 19:37:59 -0400

    Update project 3

---
+++ Project3-Countdown/NewYearCountdown/app/src/main/java/com/adam/newyearcountdown/CountdownTimer.java
@@ -0,0 +1,9 @@
+package com.adam.newyearcountdown;
+
+/**
+ * Created by Adam on 7/22/2017.
+ */
+
+public class CountdownTimer {
+
+}
--- Project3-Countdown/NewYearCountdown/app/src/main/java/com/adam/newyearcountdown/MainActivity.java
+++ Project3-Countdown/NewYearCountdown/app/src/main/java/com/adam/newyearcountdown/MainActivity.java
@@ -1,11 +1,24 @@
 package com.adam.newyearcountdown;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+
+        private ArrayAdapter<CharSequence> adapter;
+        private Spinner spinner;
+        private String countDownTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -18,14 +31,39 @@ public class MainActivity extends Activity implements View.OnClickListener {
         btnSetCount.setOnClickListener(this);
         btnStartCount.setOnClickListener(this);
 
+        spinner = (Spinner) findViewById(R.id.notifSpinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.count_times, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        spinner.setEnabled(false);
+
     }
 
     @Override
     public void onClick(View v){
         if(v.getId() == R.id.setCountBtn){
+            EditText timeInput = (EditText) findViewById(R.id.inputTime);
+            String time = timeInput.getText().toString();
 
-        }else{
+            int timeNum = Integer.parseInt(time);
 
+            if(timeNum > 5 && timeNum <= 120 && timeNum % 5 == 0){
+                countDownTime = time;
+                spinner.setEnabled(true);
+            }else{
+                spinner.setEnabled(true);
+            }
+        }else if(v.getId() == R.id.countDownBtn){
+            String spinnerItem = spinner.getSelectedItem().toString();
+            Toast.makeText(getApplicationContext(), "Spinner value: " + spinnerItem, Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Countdown has been started.", Toast.LENGTH_LONG).show();
+            new CountdownTask(getApplicationContext()).execute(countDownTime, spinnerItem);
         }
+
     }
+
 }
